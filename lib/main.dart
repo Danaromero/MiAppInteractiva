e693d4dfc296b9181277705d93c8fcf0c329bf0d
Danaromero <71467399+Danaromero@users.noreply.github.com>
2021-05-14 15:53:09 -0600

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -27,15 +27,91 @@ class _MyHomePageState extends State<PaginaInicial> {
   List<DropdownMenuItem<int>> items = [
     DropdownMenuItem(
       value: 5,
-      child: Text('asdf'),
+      child: Text('2000'),
     ),
     DropdownMenuItem(
       value: 27,
-      child: Text('qwert'),
+      child: Text('2001'),
     ),
     DropdownMenuItem(
       value: 31,
-      child: Text('yxcv'),
+      child: Text('2002'),
+    ),
+   DropdownMenuItem(
+      value: 32,
+      child: Text('2003'),
+    ),
+    DropdownMenuItem(
+      value: 33,
+      child: Text('2004'),
+    ),
+    DropdownMenuItem(
+      value: 34,
+      child: Text('2005'),
+    ),
+    DropdownMenuItem(
+      value: 35,
+      child: Text('2006'),
+    ),
+    DropdownMenuItem(
+      value: 36,
+      child: Text('2007'),
+    ),
+    DropdownMenuItem(
+      value: 37,
+      child: Text('2008'),
+    ),
+    DropdownMenuItem(
+      value: 38,
+      child: Text('2009'),
+    ),
+    DropdownMenuItem(
+      value: 39,
+      child: Text('2010'),
+    ),
+    DropdownMenuItem(
+      value: 40,
+      child: Text('2011'),
+    ),
+    DropdownMenuItem(
+      value: 41,
+      child: Text('2012'),
+    ),
+     DropdownMenuItem(
+      value: 42,
+      child: Text('2013'),
+    ),
+     DropdownMenuItem(
+      value: 43,
+      child: Text('2014'),
+    ),
+     DropdownMenuItem(
+      value: 44,
+      child: Text('2015'),
+    ),
+     DropdownMenuItem(
+      value: 45,
+      child: Text('2016'),
+    ),
+     DropdownMenuItem(
+      value: 46,
+      child: Text('2017'),
+    ),
+     DropdownMenuItem(
+      value: 47,
+      child: Text('2018'),
+    ),
+     DropdownMenuItem(
+      value: 48,
+      child: Text('2019'),
+    ),
+     DropdownMenuItem(
+      value: 49,
+      child: Text('2020'),
+    ),
+     DropdownMenuItem(
+      value: 50,
+      child: Text('2021'),
     )
   ];
 
@@ -43,7 +119,8 @@ class _MyHomePageState extends State<PaginaInicial> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('DropdownButton problem'),
+        centerTitle: true,
+        title: Text('Bienvenido a Tetos Cars'),
       ),
       body: Center(
         child: Column(
@@ -56,15 +133,15 @@ class _MyHomePageState extends State<PaginaInicial> {
             child: TextFormField(//TextFormField
               validator: (String value) {//Inicio Validator
                 if (value.isEmpty) {//IF
-                  return 'Nombre usuario Vacio';
+                  return 'Modelo del Auto';
                 } else if (value.length < 3) {//ELSE IF
                   return 'El nombre de usuario debe tener al menos 3 caracteres';
                 }
                 return null;
               },//Fin Validator
               decoration: InputDecoration(//Decorar
-                labelText: 'Nombre Usuario',
-                icon: Icon(Icons.account_circle),
+                labelText: 'Ingresa la Marca del Auto',
+                icon: Icon(Icons.edit_rounded),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20.0)),
               ),//Fin decoration
@@ -83,13 +160,15 @@ class _MyHomePageState extends State<PaginaInicial> {
                 return null;
               },//Fin Validator
               decoration: InputDecoration(//Decorar
-                labelText: 'Nombre Usuario',
-                icon: Icon(Icons.account_circle),
+                labelText: 'Ingresa el Modelo del Auto',
+                icon: Icon(Icons.drive_eta),
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20.0)),
               ),//Fin decoration
             ),//Fin TextFormField
           ),//Fin Primer Padding
+
+     
 
           Row(
         mainAxisAlignment: MainAxisAlignment.end,
@@ -97,7 +176,7 @@ class _MyHomePageState extends State<PaginaInicial> {
           SizedBox(width: 20),
                 new Expanded(
                   child: TextFormField(
-                    decoration: InputDecoration(labelText: 'Name'),
+                    decoration: InputDecoration(labelText: 'Martricula'),
                     validator: (value) {
                       return value.isEmpty ? 'Empty name.' : '';
                     },
@@ -105,7 +184,7 @@ class _MyHomePageState extends State<PaginaInicial> {
                 ),
            DropdownButton(
               items: items,
-              hint: Text("Please choose a location"),
+              hint: Text("Selecciona el Año"),
               onChanged: _enabled
                   ? (v) => setState(() {
                         value = v;
@@ -117,13 +196,17 @@ class _MyHomePageState extends State<PaginaInicial> {
         ],//Fin Widget []
       ), //Fin del primer Row
 
+        new Padding(
+                padding: const EdgeInsets.symmetric(vertical: 10.0),
+              ),
+
      Row(
-       mainAxisAlignment: MainAxisAlignment.end,
+       mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
       RaisedButton(
           color: Colors.greenAccent,
           onPressed: () => print("Button Pressed"),
-          child: new Text("Press here"),
+          child: new Text("Aceptar"),
           splashColor: Colors.red[200],
          animationDuration: Duration(seconds: 10),
          padding: EdgeInsets.all(20.0),
@@ -131,11 +214,14 @@ class _MyHomePageState extends State<PaginaInicial> {
          
                 ), //Fin RaisedButton   
 
+          new Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+              ),
 
        RaisedButton(
           color: Colors.greenAccent,
           onPressed: () => print("Button Pressed"),
-          child: new Text("Press here"),
+          child: new Text("Cancelar"),
           splashColor: Colors.red[200],
          animationDuration: Duration(seconds: 10),
          padding: EdgeInsets.all(20.0),
@@ -152,3 +238,4 @@ class _MyHomePageState extends State<PaginaInicial> {
     );
   }
 }
+
